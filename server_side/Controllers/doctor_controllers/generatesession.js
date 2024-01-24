bf4431415dbb68a59d782d7c8eef406f19a57d12
AliOding12 <aliz17043@gmail.com>
2024-01-24 09:00:00 +0500

    Add session generation controller

--- server_side/Controllers/doctor_controllers/generatesession.js
+++ server_side/Controllers/doctor_controllers/generatesession.js
@@ -4,4 +4,4 @@ function generateSessionId() {
     return crypto.randomBytes(32).toString('hex'); // Generates a 64-character hexadecimal string
 }
 
-module.exports = generateSessionId;
+module.exports = generateSessionId;// Add session generation controller
