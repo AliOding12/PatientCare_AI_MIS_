2225eda021dd31889483f89209a4946d0b0f2b0f
AliOding12 <aliz17043@gmail.com>
2025-01-17 14:00:00 +0500

    Add appointment controller

--- server_side/Controllers/app_controller/appointment.js
+++ server_side/Controllers/app_controller/appointment.js
@@ -107,3 +107,4 @@ module.exports = {appointment1,NorthKorea,bookAppointment};
 // WHERE a.doctor_id = ?
 // ORDER BY a.appointment_date ASC;
 // Add appointment controller
+// Add appointment controller
